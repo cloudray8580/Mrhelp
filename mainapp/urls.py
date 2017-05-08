e443ed298d13442de99299620410fdb9afa089f4
cloudray8580 <240078316@qq.com>
2017-05-09 02:08:25 +0800

    add  test function

---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'Administrator'
+
+
+from django.conf.urls import url
+from . import views
+from django.conf.urls import include
+
+urlpatterns = [
+    url(r'^test$', views.test, name='test')
+]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+import json
+def test(request):
+    response_data = {}
+    response_data['code'] = "200"
+    response_data['msg'] = "success"
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
