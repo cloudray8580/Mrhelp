93296c922b12f85e60a191e9613e77f84630f783
cloudray8580 <240078316@qq.com>
2017-05-09 02:45:33 +0800

    change url

--- Mrhelp/urls.py
+++ Mrhelp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^api/' , include("mainapp.urls")),
+    url(r'^admin/', admin.site.urls),
 ]
