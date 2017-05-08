cd11bb3051be7ff4caa9e64ccd208f581c3e2d00
cloudray8580 <240078316@qq.com>
2017-05-09 01:31:50 +0800

    Initial commit

---
+++ Mrhelp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Mrhelp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mrhelp.settings")
+
+application = get_wsgi_application()
