7789c0a0893f732aa84c4b17648a142c69c61817
李喆 <lizhe@lizhedeMacBook-Pro.local>
2017-06-24 16:44:44 +0800

    lalala

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Users,ThirdPlatformAccount,Mission,Chat
+from .models import Users,ThirdPlatformAccount,Mission,Chat,Activity,Advertisement
 # Register your models here.
 
 admin.site.register(Users)
 admin.site.register(ThirdPlatformAccount)
 admin.site.register(Mission)
 admin.site.register(Chat)
+admin.site.register(Activity)
+admin.site.register(Advertisement)
