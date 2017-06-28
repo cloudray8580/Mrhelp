814a7fa794b880ba6613b204012105491008c829
李喆 <lizhe@lizhedeMacBook-Pro.local>
2017-06-29 00:40:50 +0800

    fix some bugs

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,18 +4,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets
+# from rest_framework import viewsets
 
 from mainapp.models import Users
 from .serializers import UserSerializer, SystemUserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
 
 
 from django.http import HttpResponse
